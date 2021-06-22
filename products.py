5c3cf24e72f3f83a4f1c2f948276351f517e5b7b
MH <mhhsu6110@gmail.com>
2021-06-22 23:11:19 +0800

    write data into file

--- products.py
+++ products.py
@@ -4,8 +4,14 @@ while True:
 	if name == 'q':
 		break
 	price = input('please enter the price of product: ')
+	price = str(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
 	print(p[0], 'cost', p[1], 'dollars')
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,Price,unit\n')
+	for p in products:
+		f.write(p[0]+ ','+ p[1] + ',' + 'dollar' + '\n')
