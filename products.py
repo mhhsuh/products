fe79a864365c076b36651b7ac2969e38d8a6b783
MH <mhhsu6110@gmail.com>
2021-06-22 23:40:20 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'Product' in line:
+			continue
+		name, price, unit = line.strip().split(',')
+		# print(name, price)
+		products.append([name, price, unit])
+print(products)
+
+# user to key-in information
 while True:
 	name = input('please enter the name of product: ')
 	if name == 'q':
@@ -8,9 +18,11 @@ while True:
 	products.append([name, price])
 print(products)
 
+#print out all products and price information
 for p in products:
 	print(p[0], 'cost', p[1], 'dollars')
 
+#write data into file
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,Price,unit\n')
 	for p in products:
